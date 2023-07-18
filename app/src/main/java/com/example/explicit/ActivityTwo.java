436970c162d517f8b89b8ae5b2effadfe20a89ae
Rush99x <116559589+Rush99x@users.noreply.github.com>
2023-07-19 00:49:47 +0530

    First commit - upload day
    Date : 19-07-2023
    Autor : Ravindu Mihiranga

---
+++ app/src/main/java/com/example/explicit/ActivityTwo.java
@@ -0,0 +1,28 @@
+package com.example.explicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityTwo extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        button = findViewById(R.id.btnActivityTwo);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
